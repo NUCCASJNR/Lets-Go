9eb5b97f8354ad0931daf4f9f34df2ccf81887e4
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-04-28 13:41:01 +0100

    Simple arithmetics in Golang

---
+++ Go-By-Example/arithmeic.go
@@ -0,0 +1,17 @@
+// Simple arithmetic operations in Go
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Simple arithmetic operations in Go")
+
+	fmt.Println("Addition of 2 & 2 is: ", 2 + 2)
+
+	fmt.Println("Subtraction of 9 & 5 is:", 9 - 5)
+
+	fmt.Println("Multiplication of 2 and 2 is:", 2 * 2)
+
+	fmt.Println("Division of 90 by 45 is:", 180 / 45)
+}
