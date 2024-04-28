143a3347947ebfdf1a29bbeebd5dee48f21a39ba
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-04-28 22:32:16 +0100

    Constants in GO

---
+++ Go-By-Example/constant.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    const pi = 3.14159
+    const gravity = 9.8
+    const message = "Hello, world!"
+
+    fmt.Println("The value of pi is:", pi)
+    fmt.Println("The value of gravity is:", gravity)
+    fmt.Println("The message is:", message)
+}
