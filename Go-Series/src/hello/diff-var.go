07c9b3324f5c3e5353d1ef7828897d1dcb13130b
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-10-13 21:31:24 +0100

    Different vars declaration

---
+++ Go-Series/src/hello/diff-var.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		name   = "Al-Areef"
+		age    = 18
+		height int
+	)
+	fmt.Println("my name is", name)
+	fmt.Println("my age is", age)
+	fmt.Println("my height is", height)
+}
